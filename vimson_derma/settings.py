eb5917e7a59b2185f5bed5c1aac1f42b02820fe1
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-07-13 17:49:21 +0530

    updated production static files settings

--- vimson_derma/settings.py
+++ vimson_derma/settings.py
@@ -22,18 +22,18 @@ load_dotenv(dotenv_path=env_path)
 
 
 # *** CONFIGURATIONS FOR DEVELOPMENT ***
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # *** CONFIGURATIONS FOR PRODUCTION ***
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# MEDIA_URL = 'media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # where uploads are stored
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # where uploads are stored
 
 
 
