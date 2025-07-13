bb052e7cd9afccec9eff96ec6d3f573de8b59c83
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-07-13 18:08:15 +0530

    updated production urls

--- vimson_derma/settings.py
+++ vimson_derma/settings.py
@@ -32,6 +32,9 @@ load_dotenv(dotenv_path=env_path)
 # *** CONFIGURATIONS FOR PRODUCTION ***
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # where uploads are stored
 
