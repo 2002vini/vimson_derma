150e45254cf9d5c844282c1369bd4fb5043ad874
Vini Hundlani <hundlanivini2002@gmail.com>
2025-09-05 09:27:16 +0530

    fixes

--- static/js/index.js
+++ static/js/index.js
@@ -1,35 +1,3 @@
-const items = document.querySelectorAll('.carousel-item.hero');
-const nextBtn = document.querySelector('.carousel-control-next.hero');
-const prevBtn = document.querySelector('.carousel-control-prev.hero');
-
-let currentIndex = 0;
-
-function showSlide(newIndex) {
-  items.forEach((item, idx) => {
-    item.classList.remove('active', 'prev-active');
-    if (idx === currentIndex) {
-      item.classList.add('prev-active');
-    }
-  });
-
-  items[newIndex].classList.add('active');
-
-  setTimeout(() => {
-    items[currentIndex].classList.remove('prev-active');
-    currentIndex = newIndex;
-  }, 1500); // matches CSS transition duration
-}
-
-nextBtn.addEventListener('click', () => {
-  const nextIndex = (currentIndex + 1) % items.length;
-  showSlide(nextIndex);
-});
-
-prevBtn.addEventListener('click', () => {
-  const prevIndex = (currentIndex - 1 + items.length) % items.length;
-  showSlide(prevIndex);
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     const counters = document.querySelectorAll(".counter-number");
     const section = document.querySelector(".brand-section"); // Target section
@@ -75,12 +43,7 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function () {
 
     const accordionButtons = document.querySelectorAll(".accordion-button");
-    const heroSection = document.getElementById("hero-intro-section");
-    setTimeout(() => {
-      if (heroSection) {
-          heroSection.classList.add("hero-introduction-loader"); // Replace "new-class" with the desired class name
-      }
-  }, 1400); 
+
 
     accordionButtons.forEach(button => {
         button.addEventListener("click", function () {
