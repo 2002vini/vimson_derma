4a6fcd745590b4b73ba8d65cfc548f1048ccd157
Vaibhav Paliwal <vaibhavpaliwal620@gmail.com>
2025-09-08 21:54:21 +0530

    updated settings

--- vimson_derma/settings.py
+++ vimson_derma/settings.py
@@ -203,10 +203,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.hostinger.com'
 # EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER =  os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD =  os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
